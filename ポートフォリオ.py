76027e5019cebd0bde0486a746f823ba0e7aa6df
田頭　知大 <101263998+tagasu@users.noreply.github.com>
2022-03-30 23:08:16 +0900

    Add files via upload

---
+++ ポートフォリオ.py
@@ -0,0 +1,10 @@
+Python 3.8.8 (default, Apr 13 2021, 15:08:03) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 1+2
+3
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined
+>>> 
